1e07494bc3f09e065c91035fbf5c087164016b65
devkalagahamagar <devkalagahamagar@10.182.5.11>
2019-04-17 11:58:52 -0500

    first commit

---
+++ maven-demo/src/main/java/com/reature/maven_demo/TexasMule.java
@@ -0,0 +1,19 @@
+package com.reature.maven_demo;
+
+public class TexasMule {
+
+	public TexasMule() {
+		// TODO Auto-generated constructor stub
+		String obj1 = "Donkey";
+		String obj2 = "Horse";
+		System.out.println(add(obj1 , obj2));
+	}
+	public String add(String t, String s) {
+		return t + s;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TexasMule result = new TexasMule();
+	}
+
+}
